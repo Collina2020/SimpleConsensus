c0d8bc69213032546b0300f5be83dc46490a6884
Collina2020 <71427051+Collina2020@users.noreply.github.com>
2023-06-01 23:54:38 +0800

    Update message.go

--- myrpc/message.go
+++ myrpc/message.go
@@ -23,12 +23,12 @@ package myrpc
 
 type ConsensusMsg struct {
 	// Type MessageType
-	From uint8
-	Seq  uint64
-	Data []byte
-	Target	[]byte		//用difficulty来构造，要求区块计算的哈希值小于这个目标值
-	Nonce	uint64		//每个区块给自己设定的随机值，参与哈希计算
-	Hash  []byte		//用nonce算出的hash值，通过和target的比较来证明工作量
+	From   uint8
+	Seq    uint64
+	Data   []byte
+	Target []byte //用difficulty来构造，要求区块计算的哈希值小于这个目标值
+	Nonce  uint64 //每个区块给自己设定的随机值，参与哈希计算
+	Hash   []byte //用nonce算出的hash值，通过和target的比较来证明工作量
 }
 
 type ConsensusMsgReply struct {
